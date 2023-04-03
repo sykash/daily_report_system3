395ecb18c1563645afba6ec91a297cc288a115d9
hssyk <hssyk@LAPTOP-R1K5Q1S7.lan>
2023-04-04 00:36:46 +0900

    「定数ファイルを作成」

--- src/main/java/filters/EncodingFilter.java
+++ src/main/java/filters/EncodingFilter.java
@@ -10,29 +10,18 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
 
-/**
- * Servlet Filter implementation class EncodingFilter
- */
+
 @WebFilter("/*")
 public class EncodingFilter implements Filter {
 
-    /**
-     * Default constructor.
-     */
     public EncodingFilter() {
 
     }
 
-    /**
-     * @see Filter#destroy()
-     */
     public void destroy() {
 
     }
 
-    /**
-     * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
-     */
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         request.setCharacterEncoding("UTF-8");
         response.setCharacterEncoding("UTF-8");
@@ -40,9 +29,6 @@ public class EncodingFilter implements Filter {
         chain.doFilter(request, response);
     }
 
-    /**
-     * @see Filter#init(FilterConfig)
-     */
     public void init(FilterConfig fConfig) throws ServletException {
 
     }
